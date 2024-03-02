ef6cd5f1ad1a1da65fe21c5e0810f6efff2a8bdb
ccabrera-2019018 <ccabrera-2019018@kinal.edu.gt>
2024-03-01 18:00:01 -0600

    Se actualizo la coleccion

--- src/publication/publication.controller.js
+++ src/publication/publication.controller.js
@@ -24,3 +24,23 @@ export const createPublication = async (req, res) => {
     }
 }
 
+
+export const deletePublication = async(req, res)=>{
+    try{
+        let { id } = req.params;
+        let secretKey = process.env.SECRET_KEY
+        let { authorization } = req.headers
+        let { username } = jwt.verify(authorization, secretKey)
+        const publicacion = await Publication.findById(id);
+        if(publicacion.username == username){
+        let deletedPublication = await Publication.findOneAndDelete({_id: id})
+        if(!deletedPublication) return res.status(404).send({message: 'Publication not found'})
+        return res.send({message: `Publication ${deletedPublication.title} delete sucessfully`})
+    } else {
+        return res.status(400).send({ message: 'It cannot be deleted because it is not your publication.' })
+    }
+    }catch(err){
+        console.error(err)
+        return res.status(500).send({message: 'Error delete publication'})
+    }
+}
